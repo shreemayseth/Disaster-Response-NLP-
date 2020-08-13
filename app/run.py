201e83b83a460df054d8c6fc33850acf58cec787
Shreemay Kumar Setha <39726093+shreemayseth@users.noreply.github.com>
2020-08-13 23:36:29 +0530

    Update run.py

--- app/run.py
+++ app/run.py
@@ -9,7 +9,7 @@ from nltk.tokenize import word_tokenize
 from flask import Flask
 from flask import render_template, request
 from plotly.graph_objs import Bar
-from sklearn.externals import joblib
+import joblib
 from sqlalchemy import create_engine
 
 
